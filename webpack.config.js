d8b1d16c0a3bf297d3442a367211f559c89e18b9
Grzegorz Miśków <133238725+gmaxsoft@users.noreply.github.com>
2026-01-10 12:15:45 +0100

    Optymalizacja SEO i wydajności strony - Dodano kompletne meta tagi SEO: title, description, keywords, author, robots, language - Dodano Open Graph meta tagi dla Facebook: type, url, title, description, image, locale, site_name - Dodano Twitter Card meta tagi: card type, url, title, description, image - Dodano Schema.org JSON-LD structured data: Resort schema, Organization schema, BreadcrumbList schema - Dodano canonical URL i favicon/apple-touch-icon - Dodano preconnect/dns-prefetch dla zewnętrznych zasobów (Google Fonts, Google Maps, picsum.photos) - Zaimplementowano lazy loading (loading='lazy') dla wszystkich obrazów poniżej fold - Dodano width i height dla obrazów logo w celu poprawy CLS (Cumulative Layout Shift) - Zoptymalizowano Webpack: cache filesystem, TerserPlugin, CssMinimizerPlugin - Dodano splitChunks dla vendor i common chunks - Dodano runtimeChunk dla lepszego cache management - Dodano contenthash dla wszystkich assetów (images, fonts, CSS, JS) - Włączono tree shaking i minification tylko w trybie production - Dodano optymalizację obrazów: inline dla obrazów < 8kb - Skonfigurowano performance hints (maxEntrypointSize, maxAssetSize) - Cache działa poprawnie (widoczne cached assets w build output)

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,26 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
   entry: './src/js/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'js/[name].[contenthash].js',
+    chunkFilename: 'js/[name].[contenthash].chunk.js',
     clean: true,
-    assetModuleFilename: 'assets/[name][ext]',
+    assetModuleFilename: 'assets/[name].[contenthash][ext]',
   },
-  mode: 'development',
-  devtool: 'source-map',
+  mode: process.env.NODE_ENV || 'production',
+  devtool: process.env.NODE_ENV === 'development' ? 'eval-source-map' : 'source-map',
+  cache: {
+    type: 'filesystem',
+    buildDependencies: {
+      config: [__filename],
+    },
+  },
   devServer: {
     static: {
       directory: path.join(__dirname, 'dist'),
@@ -21,14 +30,25 @@ module.exports = {
     port: 9000,
     hot: true,
     open: true,
+    historyApiFallback: true,
   },
   module: {
     rules: [
       {
         test: /\.scss$/i,
         use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../',
+            },
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
           {
             loader: 'sass-loader',
             options: {
@@ -40,7 +60,15 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../',
+            },
+          },
+          'css-loader',
+        ],
       },
       {
         test: /\.js$/,
@@ -56,14 +84,19 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
         generator: {
-          filename: 'images/[name][ext]',
+          filename: 'images/[name].[contenthash][ext]',
+        },
+        parser: {
+          dataUrlCondition: {
+            maxSize: 8 * 1024, // 8kb - images smaller than 8kb will be inlined
+          },
         },
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
         generator: {
-          filename: 'fonts/[name][ext]',
+          filename: 'fonts/[name].[contenthash][ext]',
         },
       },
     ],
@@ -87,6 +120,58 @@ module.exports = {
   ],
   resolve: {
     extensions: ['.js', '.scss', '.css'],
+  },
+  optimization: {
+    minimize: process.env.NODE_ENV === 'production',
+    minimizer: process.env.NODE_ENV === 'production' ? [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true, // Remove console.log in production
+            pure_funcs: ['console.log', 'console.info', 'console.debug'],
+          },
+          format: {
+            comments: false,
+          },
+        },
+        extractComments: false,
+      }),
+      new CssMinimizerPlugin({
+        minimizerOptions: {
+          preset: [
+            'default',
+            {
+              discardComments: { removeAll: true },
+            },
+          ],
+        },
+      }),
+    ] : [],
+    splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          priority: 10,
+          reuseExistingChunk: true,
+        },
+        common: {
+          minChunks: 2,
+          priority: 5,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+    runtimeChunk: 'single',
+    moduleIds: 'deterministic',
+    usedExports: true,
+    sideEffects: false,
+  },
+  performance: {
+    hints: process.env.NODE_ENV === 'production' ? 'warning' : false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
   },
 };
 
